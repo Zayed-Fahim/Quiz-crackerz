b01c41323e9459bd2fb6a8218f972ddbb709fa7c
Zayed Fahim <zayedfahim33.cse@gmail>
2022-10-12 22:02:18 +0600

    statistics part done

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import './Header.css'
 const Header = () => {
     return (
-        <div className='flex justify-between p-7 w-screen header container mx-auto'>
+        <div className='flex justify-between p-7 min-w-screen header container mx-auto'>
             <h1 className='text-4xl font-bold text-violet-500'>Quiz 2 Go</h1>
             <nav className='text-2xl font-semibold text-violet-500 '>
                 <Link to='/'></Link>
--- src/Components/Statistics/Statistics.js
+++ src/Components/Statistics/Statistics.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { BarChart, Bar, XAxis, YAxis, Tooltip, Legend, CartesianGrid } from 'recharts';
 
 const Statistics = () => {
+    const [barChart, setBarChart] = useState([]);
+    useEffect(() => {
+        fetch("https://openapi.programming-hero.com/api/quiz")
+            .then(res => res.json())
+            .then(data => setBarChart(data.data))
+    })
+    console.log(barChart)
     return (
-        <div>
-            
+        <div className='flex justify-center my-[200px]'>
+            <BarChart width={1000} height={600} data={barChart}>
+                <XAxis dataKey="name" stroke="#8884d8" />
+                <YAxis />
+                <Tooltip wrapperStyle={{ width: 100, backgroundColor: '#ccc' }} />
+                <Legend width={100} wrapperStyle={{ top: 40, right: 20, backgroundColor: '#f5f5f5', border: '1px solid #d5d5d5', borderRadius: 3, lineHeight: '40px' }} />
+                <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+                <Bar dataKey="total" fill="#8884d8" barSize={30} />
+            </BarChart>
         </div>
     );
 };
